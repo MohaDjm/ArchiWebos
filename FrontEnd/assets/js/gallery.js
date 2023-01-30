06deacbb591bb1f15597904eafb94a2b5f411140
Mohamed <mohamedjoumoi@gmail.com>
2023-01-30 21:42:26 +0100

    ajout du dossier gallery js + filtres

---
+++ FrontEnd/assets/js/gallery.js
@@ -0,0 +1,5 @@
+// Récupération des données depuis le fichier JSON grâce a fetch
+
+const worksAPI = fetch("http://localhost:5678/api/works");
+const categoriesAPI = "http://localhost:5678/api/categories";
+
