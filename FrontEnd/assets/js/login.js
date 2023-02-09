5cfcec589087756f6635439773895ff922437fce
Mohamed <mohamedjoumoi@gmail.com>
2023-02-09 19:33:19 +0100

    feature: login page connexion

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,37 +1,40 @@
-const form = document.getElementById("login-form");
+const loginForm = document.getElementById("login-form");
 const emailInput = document.getElementById("email-input");
 const passwordInput = document.getElementById("password-input");
 const loginButton = document.getElementById("loginButton");
-const errorMessage = document.querySelector(".login-error-msg");
+const loginErrorMsg = document.querySelector(".login-error-msg");
 
-form.addEventListener("submit", (event) => {
+loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+
   const email = emailInput.value;
   const password = passwordInput.value;
 
-  fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  })
-    .then((response) => {
-      if (response.status !== 200) {
-        throw new Error("Login failed");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      localStorage.setItem("token", data.token);
-      window.location.href = "/dashboard";
-    })
-    .catch((error) => {
-        errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
-        errorMessage.style.display = "inline-block";
-        
-      loginButton.disabled = false;
-    });
+  console.log(email, password);
 
-  loginButton.disabled = true;
+  loginButton.innerHTML = " ";
+  loginButton.setAttribute("disabled", true);
+
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    loginButton.innerHTML += "Connexion en cours...";
+    if (!response.ok) {
+      throw new Error("Echec de la connexion");
+    }
+
+    const { token } = await response.json();
+    localStorage.setItem("token", token);
+    window.location.href = "./index.html";
+  } catch (error) {
+    console.log(error);
+    loginErrorMsg.style.display = "block";
+    loginButton.innerHTML+=  "Echec de la connexion";
+    loginButton.removeAttribute("disabled");
+  }
 });
